9d4e27a3d9f601d66d1d445cfcc439c58e5583d2
Logan <Logbic@outlook.com>
2025-01-22 17:24:42 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/* 
+*       File: git-lab-program.cc
+*       Author: Logan Bickerstaff
+*       Date: 01-22-2025
+*       Description: Section 111 Lab
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}//main
